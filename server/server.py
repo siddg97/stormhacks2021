c30fe484f048dc442c15702da52b1524bb44a51b
jacob-pauls <jakepauls99@gmail.com>
2021-02-20 19:44:49 -0800

    Dockerize flask app

--- server/server.py
+++ server/server.py
@@ -2,8 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/send')
+@app.route('/')
+def hello_docker():
+    return 'Flask app is running in a Docker container'
 
 @app.route('/test')
 def get_current_time():
     return "this was an endpoint test"
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
